ec11ff7f30a6032cafc63148e0ca8dc35f4e12f3
iSanty <santiago.navalon@hotmail.com>
2022-09-06 16:46:55 -0300

    probando

--- SouthApp/settings.py
+++ SouthApp/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middlewareWhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
