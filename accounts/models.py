e996d791ace9e1e5651ebf84196589c628bec955
iSanty <santiago.navalon@hotmail.com>
2022-09-15 18:35:35 -0300

    agrego cuentas de usuario

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+
 
 # Create your models here.
+
+class MasDatosUsuario(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='avatares', null=True, blank =True)
+    descripcion = RichTextField(null=True)
+    
+    def __str__(self):
+        return f'{self.user}'
+    
