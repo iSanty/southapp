542a826f4f5a94774cfef97d1644e73e949b834b
iSanty <santiago.navalon@hotmail.com>
2022-09-15 19:09:21 -0300

    issues

--- SouthApp/urls.py
+++ SouthApp/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import index
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,6 @@ urlpatterns = [
     path('informes/', include('informes.urls')),
 
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
