7beb4e9bd794364a381ec5947c57e83eacf6258a
iSanty <santiago.navalon@hotmail.com>
2022-09-15 16:33:34 -0300

    issues

--- transportes/views.py
+++ transportes/views.py
@@ -7,8 +7,8 @@ from transportes.models import Transportes
 
 class CrearTransporte(CreateView):
     model = Transportes
-    template_name = 'transportes/transportes.html'
-    success_url = 'transportes.html'
+    template_name = 'transportes.html'
+    success_url = 'index_transporte'
     fields = ['dominio', 'nombre', 'apellido', 'imagen1', 'imagen2', 'imagen3', 'imagen4', 'imagen5', 'imagen6', 'imagen7']
     
     
