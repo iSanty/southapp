6dc572c3e85aa89d8a7a72919e1d7b4a27bfbf88
iSanty <santiago.navalon@hotmail.com>
2022-09-16 12:34:47 -0300

    probando templates

--- empleados/views.py
+++ empleados/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_empleados(request):
-    return render(request, 'indexvisual.html')
+    return render(request, 'index.html')
