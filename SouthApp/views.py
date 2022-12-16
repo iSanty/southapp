0b6aca688a4a1a3af6652f97e41194cf59bad44c
iSanty <santiago.navalon@hotmail.com>
2022-12-15 22:30:39 -0300

    agregado bot whatsapp

--- SouthApp/views.py
+++ SouthApp/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from twilio.rest import Client
+from django.http.response import HttpResponse
+
+
+
+account_sid = 'AC0e085fdb45b6508a50de706b8257a337'
+auth_token = '005fb71cb00230e2e3b696db06dfc94b'
+client = Client(account_sid, auth_token)
+
+
 
 
 
 def index(request):
+    
+    if request.method =="POST":
+        mensaje = request.POST["Body"]
+        numero = request.POST["From"]
+        
+        print(numero)
+        if mensaje =="Hola":
+            client.messages.create(
+                from_='whatsapp:+14155238886',
+                body='Hola feo',
+                to=numero
+                
+            )
+        return HttpResponse('hello')
+    
     return render(request, 'index.html')
 
 
--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from twilio.rest import Client
+from django.views.decorators.csrf import csrf_exempt
+from django.http.response import HttpResponse
 
-# Create your views here.
+account_sid = 'AC0e085fdb45b6508a50de706b8257a337'
+auth_token = '005fb71cb00230e2e3b696db06dfc94b'
+client = Client(account_sid, auth_token)
+
+
+def bot(request):
+    
+    print(request)
+    return HttpResponse('hello')
